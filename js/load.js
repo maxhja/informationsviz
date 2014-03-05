25ef85df62eb0d4e1c3a5dcdd68b9585b2ab7e13
Simon Jare <simja832@student.liu.se>
2014-03-05 11:33:28 +0100

    added a loading gif

---
+++ js/load.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+  	
+	$("#intro").hide();
+
+  	/* Commented only for developing mode
+
+	// Application initalization
+	$("#main, #footer").hide();
+
+	$( "#startButton" ).click(function() {
+	  
+		$( "#intro" ).fadeOut( "slow", function() {
+			$("#main, #footer").fadeIn( "slow", function() {
+				// Fade in ready!
+			});
+		});
+	});
+
+	*/
+
+});
